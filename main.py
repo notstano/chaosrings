6d50aff9a2dab9692d2bfa90585e34f4a895793d
Stano <104390830+notstano@users.noreply.github.com>
2022-08-12 18:08:56 +0300

    Expand the rings to outside the image to contain the whole width/height
    Technically, it's not pixel-perfect so a few pixels at the edges are
    cropped.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import io
 
 import streamlit as st
-from PIL import Image
+from PIL import Image, ImageOps
 
 img1 = st.file_uploader(label='Background (original avatar) - a normal image file',
                         help='Supports Square, Portrait and Landscape orientation - the top image is centered above '
@@ -12,30 +12,39 @@ img2 = st.file_uploader(label='Overlay (chaos ring) - an image with transparency
                         help='This image should support transparency to be used as a top layer. Try using a png file.')
 '---'
 
+
+def _empty_pixel(pixel):
+    return sum(pixel[0:3]) == 0
+
+
 if img1 and img2:
-    img1 = Image.open(img1)
+    img1 = Image.open(img1).convert("RGBA")
     img2 = Image.open(img2).convert("RGBA")
-    if img2.format != 'PNG':
+
+    if img2.format and img2.format.lower() != 'png':
         st.warning(
             'The overlay image should be in a format that supports transparent parts (e.g. png) in order to be drawn'
             ' above the avatar.')
-    w1, h1 = img1.size
-    w2, h2 = img2.size
-    ratio = min(w1, h1) / min(w2, h2)
-    img2 = img2.resize((int(w2 * ratio), int(h2 * ratio)))
 
-    w1, h1 = img1.size
-    w2, h2 = img2.size
-    img1.paste(img2,
-               (w1 // 2 - w2 // 2, h1 // 2 - h2 // 2),  # center img2 in img1
-               mask=img2)
+    img2 = ImageOps.contain(img2, size=img1.size)
 
+    x = round(img2.width / 2)
+    circumference_width = sum([1 if not _empty_pixel(img2.getpixel((x, y))) else 0
+                               for y in range(0, img2.height//2)])
+
+    # The Regulars Ring is not symmetric and a little tweaking helps.
+    # 2 is for the way borders work in expand
+    # .25 is manual adjustment for the Regulars ring
+    img1 = ImageOps.expand(img1, border=round(circumference_width * 2.25))
+    img2 = ImageOps.contain(img2, size=img1.size)
+
+    img1 = Image.alpha_composite(img1, img2)
     st.image(img1)
 
     buffer = io.BytesIO()
-    img1.save(buffer, format=img1.format)
+    img1.save(buffer, format='png')
 
     st.download_button(label='Download new avatar',
                        data=buffer.getvalue(),
-                       file_name=f'new_avatar.{img1.format.lower()}',
-                       mime=f'image/{img1.format.lower()}')
+                       file_name=f'new_avatar.png',
+                       mime=f'image/png')
